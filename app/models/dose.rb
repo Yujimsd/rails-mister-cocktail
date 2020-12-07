412d05e2b322983d2d92cbdb627cfd104ccad34d
yuji masuda <yujimsd@gmail.com>
2020-12-08 06:24:58 +0900

    workrd out some bugs

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
-  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, except: [:index] do
-    resources :ingredients
-    resources :doses
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:create]
   end
-  resources :doses
+  resources :doses, only: [:destroy]
 
 end
